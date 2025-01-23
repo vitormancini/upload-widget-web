52cad983ea168c65ca9f127bc18c3119590a26e4
Vitor Mancini <vitor.mancini21@hotmail.com>
2025-01-23 17:55:56 -0300

    feat: initial visual state in components

--- src/components/upload-widget-dropzone.tsx
+++ src/components/upload-widget-dropzone.tsx
@@ -1,6 +1,10 @@
 import { useDropzone } from "react-dropzone"
+import CircularProgressBar from "./curcular-progress-bar";
 
 export function UploadWidgetDropzone () {
+    const isTherAnyPendingUpload = true;
+    const uploadGlobalPercentage = 66;
+
     const { getRootProps, getInputProps, isDragActive } = useDropzone({
         multiple: true,
         accept: {
@@ -11,17 +15,26 @@ export function UploadWidgetDropzone () {
     })
     return (
         <div className="px-3 flex flex-col gap-3">
-            <div  data-active={isDragActive}
+            <div data-active={isDragActive}
                 className="cursor-pointer text-zinc-400 bg-black/20 p-5 rounded-lg border border-zinc-700 border-dashed h-32 flex flex-col items-center justify-center gap-1 hover:border-zinc-600 transition-colors data-[active=true]:bg-indigo-500/10 data-[active=true]:border-indigo-500"
                 {...getRootProps()}
             >
                 <input type="file" {...getInputProps()} />
 
-                <span className="text-xs">Drop your files here</span>
-                <span className="text-xs underline">Click to open picker</span>
+                { isTherAnyPendingUpload ? (
+                    <div className="flex flex-col items-center gap-2.5">
+                        <CircularProgressBar progress={uploadGlobalPercentage} size={56} strokeWidth={4} />
+                        <span className="text-xs">Uploading 2 files</span>
+                    </div>
+                ) : (
+                    <>
+                        <span className="text-xs">Drop your files here</span>
+                        <span className="text-xs underline">Click to open picker</span>
+                    </>
+                )}
             </div>
 
-            <span className="text-xs text-zinc-400">Only PNG, JPG and JPEG files are supported</span>
+            <span className="text-xxs text-zinc-400">Only PNG, JPG and JPEG files are supported</span>
         </div>
     )
 }
--- src/components/upload-widget-title.tsx
+++ src/components/upload-widget-title.tsx
@@ -1,10 +1,22 @@
 import { UploadCloud } from "lucide-react";
 
 export function UploadWidgetTitle () {
+    const isTherAnyPendingUpload = true;
+    const uploadGlobalPercentage = 66;
+
     return (
         <div className="flex items-center gap-1.5 text-sm font-medium">
             <UploadCloud className="size-4" strokeWidth={1.5} />
-            <span className="text-sm font-medium">Upload files</span>
+
+            {isTherAnyPendingUpload ? (
+                <span className="flex items-baseline gap-1">
+                    Uploading
+                    <span className="text-xs text-zinc-400 tabular-nums">{ uploadGlobalPercentage }%</span>
+                </span>
+            ) : (
+                <span className="text-sm font-medium">Upload files</span>
+            )}
+
         </div>
     )
 }
