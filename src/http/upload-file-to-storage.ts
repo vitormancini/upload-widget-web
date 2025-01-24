0d2fdaa4b86294b2f5190fbd0f5ba9ce387cfa39
Vitor Mancini <vitor.mancini21@hotmail.com>
2025-01-24 14:34:24 -0300

    feat: upload file to storage with axios

---
+++ src/http/upload-file-to-storage.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+interface UploadFileToStorageParams {
+  file: File;
+}
+export async function uploadFileToStorage({ file }: UploadFileToStorageParams) {
+  const data = new FormData();
+  data.append("file", file);
+
+  const response = await axios.post<{ url: string }>("http://localhost:3000/uploads", data, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    }
+  );
+
+  return { url: response.data.url };
+}
--- src/store/uploads.ts
+++ src/store/uploads.ts
@@ -1,6 +1,7 @@
 import { create } from "zustand";
 import { enableMapSet } from "immer";
 import { immer } from "zustand/middleware/immer";
+import { uploadFileToStorage } from "../http/upload-file-to-storage";
 
 export type Upload = {
     name: string;
@@ -15,7 +16,7 @@ type UploadState = {
 enableMapSet();
 
 export const useUploads = create<UploadState, [["zustand/immer", never]]>(
-    immer((set) => {
+    immer((set, get) => {
       function addUploads(files: File[]) {
         for (const file of files) {
           const uploadId = crypto.randomUUID();
@@ -25,10 +26,22 @@ export const useUploads = create<UploadState, [["zustand/immer", never]]>(
             file,
           };
           
-        set((state) => {
-            state.uploads.set(uploadId, upload);
-          });
+          set((state) => {
+              state.uploads.set(uploadId, upload);
+            });
+
+          processUpload(uploadId);
         }
+      }
+
+      async function processUpload(uploadId: string) {
+        const upload = get().uploads.get(uploadId);
+
+        if (!upload) {
+          return;
+        }
+
+        await uploadFileToStorage({ file: upload.file })
       }
 
       return {
