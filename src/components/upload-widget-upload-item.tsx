84f68a493b4d8219657b8e9bc7ce13c4f9bef4f2
Vitor Mancini <vitor.mancini21@hotmail.com>
2025-01-26 11:36:25 -0300

    feat: compress image

--- src/components/upload-widget-upload-item.tsx
+++ src/components/upload-widget-upload-item.tsx
@@ -16,7 +16,9 @@ export function UploadWidgetUploadItem({ uploadId, upload }: UploadWidgetUploadI
     const cancelUpload = useUploads(store => store.cancelUpload);
 
     const progress = Math.min(
-        Math.round((upload.uploadSizeInBytes * 100) / upload.originalSizeInBytes),
+        upload.compressedSizeInBytes
+        ? Math.round((upload.uploadSizeInBytes * 100) / upload.compressedSizeInBytes)
+        : 0,
         100
     )
 
@@ -72,7 +74,7 @@ export function UploadWidgetUploadItem({ uploadId, upload }: UploadWidgetUploadI
                     <span className="sr-only">Download compressed image</span>
                 </Button>
 
-                <Button disabled={ upload.status !== "success" } size="icon-sm">
+                <Button disabled={ !upload.remoteUrl } onClick={() => upload.remoteUrl && navigator.clipboard.writeText(upload.remoteUrl)} size="icon-sm">
                     <Link2 className="size-4" strokeWidth={1.5} />
                     <span className="sr-only">Copy remote URL</span>
                 </Button>
--- src/store/uploads.ts
+++ src/store/uploads.ts
@@ -3,6 +3,7 @@ import { enableMapSet } from "immer";
 import { immer } from "zustand/middleware/immer";
 import { uploadFileToStorage } from "../http/upload-file-to-storage";
 import { CanceledError } from "axios";
+import { compressImage } from "../utils/compress-image";
 
 export type Upload = {
     name: string;
@@ -10,7 +11,9 @@ export type Upload = {
     abortController: AbortController;
     status: "progress" | "success" | "error" | "canceled";
     uploadSizeInBytes: number;
-    originalSizeInBytes: number
+    originalSizeInBytes: number,
+    compressedSizeInBytes?: number,
+    remoteUrl?: string
 };
 
 type UploadState = {
@@ -82,8 +85,18 @@ export const useUploads = create<UploadState, [["zustand/immer", never]]>(
         }
 
         try {
-          await uploadFileToStorage({ 
-            file: upload.file, 
+
+          const compressedFile = await compressImage({
+            file: upload.file,
+            maxHeight: 1000,
+            maxWidth: 1000,
+            quality: 0.8
+          })
+
+          updateUpload(uploadId, { compressedSizeInBytes: compressedFile.size })
+
+          const { url } = await uploadFileToStorage({ 
+            file: compressedFile, 
             onProgress(sizeInBytes) {
               updateUpload(uploadId, {
                 uploadSizeInBytes: sizeInBytes,
@@ -94,6 +107,7 @@ export const useUploads = create<UploadState, [["zustand/immer", never]]>(
 
             updateUpload(uploadId, {
               status: "success",
+              remoteUrl: url
             });
         } catch (err) {
           if (err instanceof CanceledError) {
@@ -131,8 +145,12 @@ export const usePendingUploads = () => {
 
   const { total, uploaded } = uploads.reduce(
     (acc, upload) => {
-      acc.total += upload.originalSizeInBytes;
-      acc.uploaded += upload.uploadSizeInBytes;
+      if(upload.compressedSizeInBytes) {
+        acc.uploaded += upload.uploadSizeInBytes
+      }
+
+      acc.total += upload.compressedSizeInBytes || upload.originalSizeInBytes
+
       return acc;
     },
     { total: 0, uploaded: 0 }
